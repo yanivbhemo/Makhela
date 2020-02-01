671a603ba6820f8143dc03d4c136915b1974cc79
Yaniv Ben Hamo <yaniv.bhemo@gmail.com>
2020-02-01 19:32:38 +0200

    collector.py

---
+++ collector/collector.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[57]:
+
+config_file=open()
+quit()
+import tweepy
+import json
+import requests
+import pymongo
+from pprint import pprint
+#import dnspython
+
+
+# In[14]:
+
+
+## DB Connection ##
+###DONT FORGET TO REMOVE PASSWORD###
+client = pymongo.MongoClient("mongodb+srv://yaniv:120120@makhela-qvsh8.mongodb.net/test?retryWrites=true&w=majority")
+db = client.search_stock
+
+
+# In[23]:
+
+
+## Twitter connection ##
+consumer_key = "47CFKoeTZrRdgQHY0ekYssOW4"
+consumer_secret = "MCJzt3WFJ1pWBiU0Ig2cjHz0KHkNZjK1bMi1kTrj3CoF3SGORa"
+access_token = "1129677660274286592-LL8fR1X9a94AGOY6FOQwwFFI6oTPie"
+access_token_secret = "kYg9f68bQgBJnctrymcQQcDE282wvPn0EBEWH5DDzZNvN"
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+# In[64]:
+
+
+influencers_names = ["ArianeTabatabai","SuzanneMaloney","MehdiKhalaji","Alireza Nader","Dalia Dassa Kaye","Karim Sadjadpour","Richard (Dick) Sokolsky","George Perkovich","Mark Hibbs","Suzanne DiMaggio","Mark Dubowitz","Reuel Mark Gerecht","Richard Goldberg","Olli Heinonen","Pierre Goldschmidt","David Albright","Emanuele Ottolenghi","Juan Zarate","Robert Einhorn","Djavad Salehi-Isfahani","Norman T. Roule","Dennis B. Ross","Colin Kahl","Jon B. Wolfstahl","Gary Samore","Jeffrey Lewis","Ray Takeyh","Vali Nasr","Graham Allison","Michael Eisenstadt","Barbara A. Leaf","Patrick Clawson","Anthony Cordesman","Barbara Slavin","Trita Parsi","Mohammad Ali Shabani","Ellie Geranmayeh","Esfandyar Batmanghelidj","Ali Vaez","Shahram Chubin","Michael Eisenstadt","Ali Alfoneh","Matthew Kroenig","Barbara Slavin","Holly Dagres","Jerrold D.Green","John Calabrese","William Yong","Ahmad Majidyar","Afshon Ostovar","Mark Fitzpatrick","Kenneth M. Pollack","Michael Elleman","Raz  Zimmt","Tal Inbar","Uzi Rubin","Sima Schein","Amos Yadlin","Emily Landau","Ephraim Asculai","Ariel (Eli) Levite"]
+for person in influencers_names:
+    print("Search for: " + person)
+    try:
+        user = api.get_user(person)
+        print("Found! User id: " + str(user.id))
+    except tweepy.TweepError as e:
+        pprint(e)  # prints 34
+        break
+
+
+# In[5]:
+
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+
+
+# In[19]:
+
+
+user_id = api.get_user("realdonaldtrump").id
+
+
+# In[20]:
+
+
+user_tweets = api.user_timeline(user_id,count=1)
+
+
+# In[14]:
+
+
+print(user_tweets)
+
+
+# In[35]:
+
+
+for tweet in user_tweets:
+    print("User - " + str(user_id))
+    print("Id - " + str(tweet.id))
+    print("Created_at - " + str(tweet.created_at))
+    print("Text - " + str(tweet.text))
+
+
+# In[8]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
