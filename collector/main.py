aef03c7cb124ce33696da8cea19e7f0ced5d37b2
Ben Hamo <Yaniv.BenHamo@emc.com>
2020-03-05 18:57:09 +0200

    start working on collecting posts

--- collector/main.py
+++ collector/main.py
@@ -1,11 +1,15 @@
 from source_handlers import twitter
 import collector as clt
 
+
 def main():
     twitter_handler = twitter.Twitter_handler()
     collector = clt.Collector(twitter_handler)
     collector.collect()
 
+
 if __name__ == "__main__":
     main()
     exit(0)
+
+## TEST after stash ##
