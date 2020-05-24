512809ca432e386abf0033e1ca2cb90caa9c931f
yanivbhemo <yaniv.bhemo@gmail.com>
2020-05-24 14:40:02 +0300

    keywords section added

--- ui/app/src/components/Pages/Settings/index.js
+++ ui/app/src/components/Pages/Settings/index.js
@@ -4,7 +4,7 @@ import Menu from '../../Menu'
 import Content from '../../Content'
 import Row from '../../Row'
 import Col from '../../Col'
-import UsersPanel,{InitSystemPanel, ThresholdPanel, SettingItem} from './Panel'
+import UsersPanel,{InitSystemPanel, ThresholdPanel, SettingItem, KeywordPanel, KeywordItem} from './Panel'
 import ModalBox from '../../ModalBox'
 import Cookies from 'js-cookie';
 import * as CONSTS from '../../../consts'
@@ -60,6 +60,53 @@ function SettingsPage(props) {
         )
     }
 
+    const [keywords, setKeywords] = useState([])
+    useEffect(() => {
+        const url = CONSTS.GET_ALL_KEYWORDS
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({"token":Cookies.get('token')}),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+            .then(data => {
+                setKeywords(data)
+            })
+            .catch(err => console.log(err))
+        .catch(err => console.log(err))
+    }, [])
+
+    function eachKeyword(keyword, id) {
+        return(
+            <KeywordItem
+                key={`keyword${id}`}
+                word={keyword.word}
+                onDltBtnClick={deleteKeyword}
+            />
+        )
+    }
+
+    function deleteKeyword(word)
+    {
+        const url = CONSTS.DELETE_KEYWORD
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({"token":Cookies.get('token'), word: word}),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+        .then(res => {
+            if(res.status === 200) {
+                toggleModel({modalStatus: true, blackBackground: "block", modalType: 'success', modalText:'Changes Saved Successfuly'})
+                setKeywords(keywords.filter(item => item.word !== word))
+            }
+        })
+        .catch(err => console.log(err))
+    }
+
     function saveSetting(attribute, value)
     {
         const url = CONSTS.UPDATE_SETTING
@@ -77,6 +124,24 @@ function SettingsPage(props) {
         })
         .catch(err => console.log(err))
     }
+
+    function addKeyword(word)
+    {
+        const url = CONSTS.ADD_KEYWORD
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({"token":Cookies.get('token'), word:word}),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+        .then(res => {
+            if(res.status === 200) {
+                toggleModel({modalStatus: true, blackBackground: "block", modalType: 'success', modalText:'Keyword "' + word + '" Added successfuly'})
+            }
+        })
+        .catch(err => console.log(err))
+    }
     
     return(
         <React.Fragment>
@@ -90,7 +155,7 @@ function SettingsPage(props) {
                         </UsersPanel>
                     </Col>
                 </Row>
-                <Row className="row mt">
+                <Row className="row">
                     <Col className="col-md-12">
                         <ThresholdPanel 
                         title="Collector and Suggestions Thresholds"
@@ -99,7 +164,14 @@ function SettingsPage(props) {
                         </ThresholdPanel>
                     </Col>
                 </Row>
-                <Row className="row mt">
+                <Row className="row">
+                    <Col className="col-md-12">
+                        <KeywordPanel title="Keywords To Search For" onAddClick={addKeyword}>
+                            {keywords.map((item,i) => eachKeyword(item, i))}
+                        </KeywordPanel>
+                    </Col>
+                </Row>
+                <Row className="row">
                     <Col className="col-md-12">
                         <InitSystemPanel title="Initilize System" onClick={()=>toggleModel({modalStatus: true, blackBackground: "block", modalType: 'danger', modalText:'Are you sure? All data will be lost'})}>
                             Test
