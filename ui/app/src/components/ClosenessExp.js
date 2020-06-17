cd3c0457f8b219f2ecff13139760e0e4b6b31cc7
Sveta Gimpelson <37687300+Svetagim@users.noreply.github.com>
2020-06-17 09:24:28 +0300

    Update ClosenessExp.js

--- ui/app/src/components/ClosenessExp.js
+++ ui/app/src/components/ClosenessExp.js
@@ -1,5 +1,5 @@
 module.exports = {
-    degree: 'Displays influencers significance according to number of people following him.',
+    degree: 'Displays influencers significance according to number of people following him',
     betweenness: 'Displays the "short path" connection number between two people passing through the influencer',
     closeness: 'Displays the average of the "shortest paths" from the influencer to other people on the network connected to him'
 }
