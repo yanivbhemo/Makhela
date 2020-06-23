27b990dd9f8de7df75454a4c605f59b2777b1e0f
yanivbhemo <yaniv.bhemo@gmail.com>
2020-06-23 20:26:03 +0300

    fix origin

--- ui/collector-service/server.js
+++ ui/collector-service/server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.set('json spaces', 2);
 
 app.use(   (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://makhela.live");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, Content-Type, Accept");
     res.set("Content-Type", "application/json");
     next();
